487734ec1bd9cd6794f4d71dc0b888cb70c87d45
mammumalluru <mamatha.malluru@outlook.com>
2025-12-03 12:51:57 +0530

    intern

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,9 @@
+// module.exports = {
+//   url: "mongodb://localhost:27017/dd_db"
+// };
+
+
+// app/config/db.config.js
 module.exports = {
-  url: "mongodb://localhost:27017/dd_db"
+  url: process.env.MONGO_URL || "mongodb://localhost:27017/dd_db"
 };
